ed23dbffebe6e857b6dec9e3884b0b7fcaba94f8
Tassawer Hussain <t.tassawer@gmail.com>
2025-10-30 20:52:15 +0500

    Update the admin post list table overview

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
 from .models import Post
 
+# admin.site.register(Post)
+
 # Register your models here.
-admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+	# used in admin panel to display the fields
+	list_display = ['title', 'slug', 'author', 'publish', 'status']
+	list_filter = ['status', 'created', 'publish', 'author'] # used to filter the fields
+	search_fields = ['title', 'body'] # used to search the fields
+	prepopulated_fields = {'slug': ('title',)} # used to populate the slug field from the title field
+	raw_id_fields = ['author'] # used to display the author field as a dropdown
+	date_hierarchy = 'publish' # used to display the publish field as a dropdown
+	ordering = ['status', 'publish'] # used to order the fields
